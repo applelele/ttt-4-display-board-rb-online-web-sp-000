2f0347a51937ce8d4cac35eec35d09a8681627b1
YUKIKO IKEDA <applelele@gmail.com>
2020-06-08 22:25:49 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -184,7 +184,7 @@ describe "#display_board in 'lib/display_board.rb" do
       # example's situation.
 
 
-      board = ["O", "O", "O", "O", "O", "O" "O", "O", "O"] # This is not correct
+      board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"] # This is not correct
 
       output = capture_puts{ display_board(board) } if defined?(display_board)
       rows = output.split("\n")
